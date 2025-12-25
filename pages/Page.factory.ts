dfbcaee4b66bad75ebc1be1c887e885e5f9bfb7f
YordanPopov <jordanpopov190@gmail.com>
2025-12-25 20:59:17 +0200

    add login page object

--- pages/Page.factory.ts
+++ pages/Page.factory.ts
@@ -2,11 +2,14 @@ import { Page } from '@playwright/test';
 
 /* Page objects */
 import SignUpPage from '@pages/SignUp.page';
+import LogInPage from '@pages/LogIn.page';
 
 export default class PageFactory {
     readonly signUpPage: SignUpPage;
+    readonly logInPage: LogInPage;
 
     constructor(protected page: Page) {
         this.signUpPage = new SignUpPage(page);
+        this.logInPage = new LogInPage(page);
     }
 }
