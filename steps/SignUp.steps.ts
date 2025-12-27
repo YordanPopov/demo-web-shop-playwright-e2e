2a310d8f74b9299dad8ee058abfbfbf23990cda4
YordanPopov <jordanpopov190@gmail.com>
2025-12-27 23:03:13 +0200

    add attemptRegisterUser method

--- steps/SignUp.steps.ts
+++ steps/SignUp.steps.ts
@@ -1,23 +1,33 @@
-import { Page } from '@playwright/test';
+import { Page, expect } from '@playwright/test';
 
 /* Page objects */
 import PageFactory from '@pages/Page.factory';
+
+/* Types */
+import type { RegistrationData } from '@test-data/users.data';
+import type { ValidationErrors } from '@test-data/users.data';
 
 export default class SignUpSteps extends PageFactory {
     constructor(page: Page) {
         super(page);
     }
 
-    async registerUser(data: {
-        gender?: 'male' | 'female';
-        firstName: string;
-        lastName: string;
-        email: string;
-        password: string;
-        confirmPassword: string;
-    }): Promise<void> {
+    async registerUser(data: RegistrationData): Promise<void> {
         await this.signUpPage.register(data);
 
         await this.signUpPage.verifyRegistration();
+    }
+
+    async attemptRegisterUser(
+        invalidData: RegistrationData,
+        expectedErrors: ValidationErrors
+    ): Promise<void> {
+        await this.signUpPage.register(invalidData);
+
+        const actualErrors = await this.signUpPage.getValidationErrors();
+
+        for (const field in expectedErrors) {
+            await expect(actualErrors[field]).toBe(expectedErrors[field]);
+        }
     }
 }
