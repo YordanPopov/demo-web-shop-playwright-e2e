ed550e385d3ec3872ef2738e2c4b534b067cb6fb
YordanPopov <jordanpopov190@gmail.com>
2025-12-31 16:43:23 +0200

    add invalid login data

--- fixtures/test-data/logIn.data.ts
+++ fixtures/test-data/logIn.data.ts
@@ -1,4 +1,4 @@
-import { LoginTestCase } from '@types';
+import { invalidLoginTestCase, LoginTestCase } from '@types';
 
 export const VALID_LOGIN_USERS: LoginTestCase[] = [
     {
@@ -22,6 +22,49 @@ export const VALID_LOGIN_USERS: LoginTestCase[] = [
         data: {
             email: 'tester_yo@email.com',
             password: 'test1234',
+        },
+    },
+];
+
+export const INVALID_LOGIN_DATA: invalidLoginTestCase[] = [
+    {
+        description: 'with empty fields.',
+        data: {
+            email: '',
+            password: '',
+        },
+        expectedError: {
+            global: 'Login was unsuccessful. Please correct the errors and try again.\nNo customer account found',
+        },
+    },
+    {
+        description: 'with empty email field.',
+        data: {
+            email: '',
+            password: 'P@$$1234',
+        },
+        expectedError: {
+            global: 'Login was unsuccessful. Please correct the errors and try again.\nNo customer account found',
+        },
+    },
+    {
+        description: 'with empty password field.',
+        data: {
+            email: 'tester_yo@email.com',
+            password: '',
+        },
+        expectedError: {
+            global: 'Login was unsuccessful. Please correct the errors and try again.\nThe credentials provided are incorrect',
+        },
+    },
+    {
+        description: 'with wrong password.',
+        data: {
+            email: 'tester_yo@email.com',
+            password: 'wr0ngP@ssw0rd',
+        },
+        expectedError: {
+            global: 'Login was unsuccessful. Please correct the errors and try again.\nThe credentials provided are incorrect',
         },
     },
 ];
