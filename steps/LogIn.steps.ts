b107c94b0d3e088daa89e563690952173f1ae2ae
YordanPopov <jordanpopov190@gmail.com>
2025-12-31 16:45:35 +0200

    implement attemptLoginUser() method

--- steps/LogIn.steps.ts
+++ steps/LogIn.steps.ts
@@ -1,19 +1,29 @@
-import { Page } from '@playwright/test';
+import { expect, Page } from '@playwright/test';
 
 /* Page objects */
 import PageFactory from '@pages/Page.factory';
 
 /* Types */
-import type { LogInData } from '@types';
+import type { LogInData, LoginValidationErrors } from '@types';
 
 export default class LogInSteps extends PageFactory {
     constructor(page: Page) {
         super(page);
     }
 
-    async login(data: LogInData): Promise<void> {
+    async loginUser(data: LogInData): Promise<void> {
         await this.logInPage.login(data);
 
         await this.logInPage.verifySuccessfulLogin();
+    }
+
+    async attemptLoginUser(data: LogInData, expectedErrors: LoginValidationErrors): Promise<void> {
+        await this.logInPage.login(data);
+
+        const actualErrors = await this.logInPage.getValidationErrors();
+
+        for (const field in expectedErrors) {
+            await expect(actualErrors[field]).toBe(expectedErrors[field]);
+        }
     }
 }
