58215ee0b4d50845b4bfb592420d5d5b07d4a264
YordanPopov <jordanpopov190@gmail.com>
2026-01-04 22:15:18 +0200

    add searchResultsPage

--- pages/Page.factory.ts
+++ pages/Page.factory.ts
@@ -4,15 +4,18 @@ import { Page } from '@playwright/test';
 import HomePage from '@pages/Home.page';
 import SignUpPage from '@pages/SignUp.page';
 import LogInPage from '@pages/LogIn.page';
+import SearchResultsPage from '@pages/SearchResults.page';
 
 export default class PageFactory {
     readonly homePage: HomePage;
     readonly signUpPage: SignUpPage;
     readonly logInPage: LogInPage;
+    readonly searchResultsPage: SearchResultsPage;
 
     constructor(protected page: Page) {
         this.homePage = new HomePage(page);
         this.signUpPage = new SignUpPage(page);
         this.logInPage = new LogInPage(page);
+        this.searchResultsPage = new SearchResultsPage(page);
     }
 }
