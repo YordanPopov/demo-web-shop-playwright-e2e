b2ef5a362c8893c29ffc3fd9e92c7caa9824a40c
YordanPopov <jordanpopov190@gmail.com>
2026-01-11 20:55:58 +0200

    add navigation methods

--- components/Footer.component.ts
+++ components/Footer.component.ts
@@ -12,6 +12,12 @@ export class FooterComponent {
     private readonly contactUsLink: Locator;
 
     /* Customer service links */
+    private readonly searchLink: Locator;
+    private readonly newsLink: Locator;
+    private readonly blogLink: Locator;
+    private readonly recentlyViewedProductsLink: Locator;
+    private readonly compareProductsList: Locator;
+    private readonly newProducts: Locator;
 
     /* My account links */
     private readonly myAccountLink: Locator;
@@ -21,11 +27,16 @@ export class FooterComponent {
     private readonly wishlistLink: Locator;
 
     /* Follow us links */
+    private readonly facebook: Locator;
+    private readonly twitter: Locator;
+    private readonly rss: Locator;
+    private readonly youtube: Locator;
+    private readonly googlePlus: Locator;
 
     constructor(page: Page) {
         this.page = page;
 
-        /* Information */
+        /* Information links */
         this.siteMapLink = page.locator('.footer a[href="/sitemap"]');
         this.shippingReturnsLink = page.locator('.footer a[href="/shipping-returns"]');
         this.privacyNoticeLink = page.locator('.footer a[href="/privacy-policy"]');
@@ -33,11 +44,59 @@ export class FooterComponent {
         this.aboutUsLink = page.locator('.footer a[href="/about-us"]');
         this.contactUsLink = page.locator('.footer a[href="/contactus"]');
 
-        /* Account */
+        /* Customer service links */
+        this.searchLink = page.locator('.footer a[href="/search"]');
+        this.newsLink = page.locator('.footer a[href="/news"]');
+        this.blogLink = page.locator('.footer a[href="/blog"]');
+        this.recentlyViewedProductsLink = page.locator('.footer a[href="/recentlyviewedproducts"]');
+        this.compareProductsList = page.locator('.footer a[href="/compareproducts"]');
+        this.newProducts = page.locator('.footer a[href="/newproducts"]');
+
+        /* My account */
         this.myAccountLink = page.locator('.footer a[href="/customer/info"]');
         this.ordersLink = page.locator('.footer a[href="/customer/orders"]');
         this.addressesLink = page.locator('.footer a[href="/customer/addresses"]');
         this.shoppingCartLink = page.locator('.footer a[href="/cart"]');
         this.wishlistLink = page.locator('.footer a[href="/wishlist"]');
+
+        /* Follow us links */
+        this.facebook = page.locator('.footer li.facebook a');
+        this.twitter = page.locator('.footer li.twitter a');
+        this.rss = page.locator('.footer li.rss a');
+        this.youtube = page.locator('.footer li.youtube a');
+        this.googlePlus = page.locator('.footer li.google-plus a');
+    }
+
+    /* Navigation methods */
+    async clickSearch(): Promise<void> {
+        await this.searchLink.click();
+    }
+
+    async clickNews(): Promise<void> {
+        await this.newsLink.click();
+    }
+
+    async clickRecentlyViewedProducts(): Promise<void> {
+        await this.recentlyViewedProductsLink.click();
+    }
+
+    async clickCompareProductsList(): Promise<void> {
+        await this.compareProductsList.click();
+    }
+
+    async clickMyAccount(): Promise<void> {
+        await this.myAccountLink.click();
+    }
+
+    async clickOrders(): Promise<void> {
+        await this.ordersLink.click();
+    }
+
+    async clickShoppingCart(): Promise<void> {
+        await this.shoppingCartLink.click();
+    }
+
+    async clickWishlist(): Promise<void> {
+        await this.wishlistLink.click();
     }
 }
