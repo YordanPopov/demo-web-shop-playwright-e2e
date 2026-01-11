fa8198e2f7be34012c61f75f4fec40e58d0cc6fa
YordanPopov <jordanpopov190@gmail.com>
2026-01-11 20:56:10 +0200

    add type Category

---
+++ types/categories.types.ts
@@ -0,0 +1,12 @@
+export type Category =
+    | 'books'
+    | 'computers'
+    | 'desktops'
+    | 'notebooks'
+    | 'accessories'
+    | 'electronics'
+    | 'cell-phones'
+    | 'apparel-shoes'
+    | 'digital-downloads'
+    | 'jewelry'
+    | 'gift-cards';
