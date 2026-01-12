c0817a1211a1e9ec826e18ed2ebd94c4ecf1159f
YordanPopov <jordanpopov190@gmail.com>
2026-01-12 21:10:09 +0200

    add categories data

---
+++ fixtures/test-data/categories.data.ts
@@ -0,0 +1,13 @@
+import type { Categories } from '@types';
+
+export const CATEGORIES: Categories[] = [
+    { name: 'Books', slug: 'books' },
+    { name: 'Desktops', slug: 'desktops' },
+    { name: 'Notebooks', slug: 'notebooks' },
+    { name: 'Accessories', slug: 'accessories' },
+    { name: 'Camera, photo', slug: 'camera-photo' },
+    { name: 'Apparel & Shoes', slug: 'apparel-shoes' },
+    { name: 'Digital downloads', slug: 'digital-downloads' },
+    { name: 'Jewelry', slug: 'jewelry' },
+    { name: 'Gift Cards', slug: 'gift-cards' },
+];
