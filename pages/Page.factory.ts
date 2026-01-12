06212bffdc08ef4debb9f8351906a6de7c6dcfb8
YordanPopov <jordanpopov190@gmail.com>
2026-01-12 21:11:05 +0200

    add categoryPage

--- pages/Page.factory.ts
+++ pages/Page.factory.ts
@@ -5,17 +5,20 @@ import HomePage from '@pages/Home.page';
 import SignUpPage from '@pages/SignUp.page';
 import LogInPage from '@pages/LogIn.page';
 import SearchResultsPage from '@pages/SearchResults.page';
+import CategoryPage from '@pages/Category.page';
 
 export default class PageFactory {
     readonly homePage: HomePage;
     readonly signUpPage: SignUpPage;
     readonly logInPage: LogInPage;
     readonly searchResultsPage: SearchResultsPage;
+    readonly categoryPage: CategoryPage;
 
     constructor(protected page: Page) {
         this.homePage = new HomePage(page);
         this.signUpPage = new SignUpPage(page);
         this.logInPage = new LogInPage(page);
         this.searchResultsPage = new SearchResultsPage(page);
+        this.categoryPage = new CategoryPage(page);
     }
 }
