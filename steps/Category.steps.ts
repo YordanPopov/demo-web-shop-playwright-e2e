8944c675f2b91d743f68ef2b6501896f5b8ad173
YordanPopov <jordanpopov190@gmail.com>
2026-01-13 21:07:25 +0200

    add sort methods

--- steps/Category.steps.ts
+++ steps/Category.steps.ts
@@ -4,7 +4,7 @@ import { expect, Page } from '@playwright/test';
 import PageFactory from '@pages/Page.factory';
 
 /* Types */
-import { Category } from '@types';
+import { Category, SortByOptions } from '@types';
 
 export default class CategorySteps extends PageFactory {
     constructor(page: Page) {
@@ -21,5 +21,19 @@ export default class CategorySteps extends PageFactory {
 
         const count = await this.categoryPage.getProductsCount();
         expect(count).toBeGreaterThan(0);
+    }
+
+    async sortProducts(option: SortByOptions): Promise<void> {
+        await this.categoryPage.sortBy(option);
+    }
+
+    async verifySorting(type: 'name' | 'price', order: 'asc' | 'desc'): Promise<void> {
+        if (type === 'name') {
+            const isValid = await this.categoryPage.verifySortByName(order);
+            expect(isValid).toBeTruthy();
+        } else {
+            const isValid = await this.categoryPage.verifySortByPrice(order);
+            expect(isValid).toBeTruthy();
+        }
     }
 }
