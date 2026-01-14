d03af8ca793f697efeb214055a191564f2d7b4b3
YordanPopov <jordanpopov190@gmail.com>
2026-01-14 21:30:14 +0200

    add categories types

--- types/categories.types.ts
+++ types/categories.types.ts
@@ -1,4 +1,4 @@
-export type Category =
+export type CategorySlug =
     | 'books'
     | 'computers'
     | 'desktops'
@@ -12,7 +12,26 @@ export type Category =
     | 'jewelry'
     | 'gift-cards';
 
-export interface Categories {
+export type SortOptions =
+    | 'Position'
+    | 'Name: A to Z'
+    | 'Name: Z to A'
+    | 'Price: Low to High'
+    | 'Price: High to Low'
+    | 'Created on';
+
+export type SortType = 'name' | 'price';
+export type SortOrder = 'asc' | 'desc';
+
+export interface CategoryInfo {
     name: string;
-    slug: Category;
+    slug: CategorySlug;
+    hasSubCategories?: boolean;
+    parentCategory?: string;
+}
+
+export interface SortTestCase {
+    label: SortOptions;
+    sortType: SortType;
+    sortORder: SortOrder;
 }
