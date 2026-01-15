44df4aa0a3fbb6843fb784777380e6b13fcfd538
YordanPopov <jordanpopov190@gmail.com>
2026-01-15 18:59:42 +0200

    add categories display and sort tests

--- tests/categories.spec.ts
+++ tests/categories.spec.ts
@@ -1,13 +1,47 @@
 import { test } from '@steps/Step.factory';
-import { CATEGORIES } from '@test-data/categories.data';
+import { ALL_CATEGORIES, SORT_TEST_CASE } from '@test-data/categories.data';
 
-test.describe('Category Pages - All Categories', () => {
-    for (const category of CATEGORIES) {
-        test.describe(`${category.name} Category`, () => {
-            test('Display products', { tag: '@category' }, async ({ categorySteps }) => {
+test.describe('Category Pages - Product Display', () => {
+    for (const category of ALL_CATEGORIES) {
+        test(
+            `${category.name} - should display products`,
+            {
+                tag: ['@category', '@smoke'],
+                annotation: [
+                    { type: 'Category', description: category.name },
+                    { type: 'CategorySlug', description: category.slug },
+                ],
+            },
+            async ({ categorySteps }) => {
                 await categorySteps.navigateToCategory(category.slug);
                 await categorySteps.verifyProductsDisplayed();
-            });
+            }
+        );
+    }
+});
+
+test.describe('Category Pages - Product Sorting', () => {
+    for (const category of ALL_CATEGORIES) {
+        test.describe(`${category.name}`, () => {
+            for (const sortCase of SORT_TEST_CASE) {
+                test(
+                    `Sort by ${sortCase.label}`,
+                    {
+                        tag: ['@category', '@sorting'],
+                        annotation: [
+                            { type: 'Category', description: category.name },
+                            { type: 'SortOption', description: sortCase.label },
+                            { type: 'SortType', description: sortCase.sortType },
+                            { type: 'sortOrder', description: sortCase.sortORder },
+                        ],
+                    },
+                    async ({ categorySteps }) => {
+                        await categorySteps.navigateToCategory(category.slug);
+                        await categorySteps.sortProducts(sortCase.label);
+                        await categorySteps.verifySorting(sortCase.sortType, sortCase.sortORder);
+                    }
+                );
+            }
         });
     }
 });
