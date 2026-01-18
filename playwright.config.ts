ba20d1c988dd5dc89e7141f5c05226214f54b39f
YordanPopov <jordanpopov190@gmail.com>
2026-01-18 21:28:33 +0200

    update Playwright configuration: adjust retries, add HTML reporter, and increase slowMo delay

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,11 +18,13 @@ export default defineConfig({
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
+    //retries: process.env.CI ? 2 : 0,
+    retries: 1,
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: [['allure-playwright'], ['list', { open: 'always' }]],
+    reporter: [['allure-playwright'], ['list', { open: 'always' }], ['html', { open: 'never' }]],
+    timeout: 30_000,
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('')`. */
@@ -32,7 +34,7 @@ export default defineConfig({
         trace: 'on-first-retry',
 
         launchOptions: {
-            slowMo: 500,
+            slowMo: 1000,
         },
     },
     /* Configure projects for major browsers */
