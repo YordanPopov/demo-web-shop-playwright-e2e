b9ca6154aa6d5f4958695f10e7147b956f0ded4e
YordanPopov <jordanpopov190@gmail.com>
2026-02-05 18:16:31 +0200

    add Address interface and types for ShippingMethod and PaymentMethod

---
+++ types/checkout.types.ts
@@ -0,0 +1,21 @@
+export interface Address {
+    fisrtName: string;
+    lastName: string;
+    email: string;
+    company?: string;
+    country: string;
+    state?: string;
+    city: string;
+    address1: string;
+    address2?: string;
+    postalCode: string;
+    phoneNumber: string;
+    fax?: string;
+}
+
+export type ShippingMethod = 'Ground (0.00)' | 'Next Day Air (0.00)' | '2nd Day Air (0.00)';
+export type PaymentMethod =
+    | 'Cash On Delivery (COD) (7.00)'
+    | 'Check / Money Order (5.00)'
+    | 'Credit Card'
+    | 'Purchase Order';
