8e6344c2894d18cbb7e03bd0017c66628c32af8f
YordanPopov <jordanpopov190@gmail.com>
2026-03-30 20:12:23 +0300

    feat: add tests for checkout process with new addresses

--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -4,6 +4,7 @@ import {
     CHECKOUT_USER,
     DEFAULT_PRODUCT,
     SAVED_ADDRESS_CHECKOUT_CASES,
+    NEW_ADDRESS_CHECKOUT_CASES,
 } from '@test-data/checkout.data';
 
 test.describe('Checkout - Saved Addresses', () => {
@@ -21,13 +22,63 @@ test.describe('Checkout - Saved Addresses', () => {
     });
 
     for (const testCase of SAVED_ADDRESS_CHECKOUT_CASES) {
-        test(testCase.description, { tag: ['@checkout', '@saved'] }, async ({ checkoutSteps }) => {
-            await checkoutSteps.completeCheckoutWithSavedAdresses(
-                testCase.expectedPaymentInfo,
-                testCase.expectedOrderSummary,
-                testCase.shippingMethod,
-                testCase.paymentMethod
-            );
-        });
-    } 
+        test(
+            testCase.description,
+            {
+                tag: ['@checkout', '@saved'],
+                annotation: [
+                    { type: 'Shipping method', description: testCase.shippingMethod },
+                    { type: 'Payment method', description: testCase.paymentMethod },
+                    { type: 'Expected payment info', description: testCase.expectedPaymentInfo },
+                ],
+            },
+            async ({ checkoutSteps }) => {
+                await checkoutSteps.completeCheckoutWithSavedAdresses(
+                    testCase.expectedPaymentInfo,
+                    testCase.expectedOrderSummary,
+                    testCase.shippingMethod,
+                    testCase.paymentMethod
+                );
+            }
+        );
+    }
+});
+
+test.describe('Checkout - New Address', () => {
+    test.beforeEach(async ({ sharedSteps, logInSteps, categorySteps, productSteps, cartSteps }) => {
+        await sharedSteps.navigateToLoginPage();
+        await logInSteps.loginUser(CHECKOUT_USER);
+
+        await categorySteps.navigateToCategory(DEFAULT_PRODUCT.category);
+        await categorySteps.openProductByTitle(DEFAULT_PRODUCT.productTitle);
+
+        await productSteps.addProductToCart(DEFAULT_PRODUCT.qty);
+
+        await cartSteps.openCart();
+        await cartSteps.proceedToCheckout();
+    });
+
+    for (const testCase of NEW_ADDRESS_CHECKOUT_CASES) {
+        test(
+            testCase.description,
+            {
+                tag: ['@checkout', '@new'],
+                annotation: [
+                    { type: 'Shipping method', description: testCase.shippingMethod },
+                    { type: 'Payment method', description: testCase.paymentMethod },
+                    { type: 'Expected payment info', description: testCase.expectedPaymentInfo },
+                ],
+            },
+            async ({ checkoutSteps }) => {
+                await checkoutSteps.completeCheckoutWithNewAddresses(
+                    testCase.billingAddress,
+                    testCase.shippingAddress,
+                    testCase.expectedPaymentInfo,
+                    testCase.expectedOrderSummary,
+                    testCase.shippingMethod,
+                    testCase.paymentMethod
+                );
+            }
+        );
+    }
 });
